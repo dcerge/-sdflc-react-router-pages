c4b5add52ec07e2b261717a25671e2125fbb2a6d
Sergey Demidenko <dcerge@gmail.com>
2021-05-05 13:44:35 -0600

    Configuration changes

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import postcss from 'rollup-plugin-postcss';
 import url from '@rollup/plugin-url';
 import json from '@rollup/plugin-json';
 
-const packageJson = require('./package.json');
+import packageJson from './package.json';
 
 export default {
   input: 'src/index.js',
